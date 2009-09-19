1ba6cbbc4c6c6cb816723dcc3147c9c5762f8621
Joshua May <notjosh@gmail.com>
2009-09-18 23:32:42 -0700

    can save the position and size of blocks now

--- apps/frontend/modules/block/templates/indexSuccess.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($blocks as $block): ?>
-	<p><?php echo $block->get('title') ?></p>
-<?php endforeach ?>
--- apps/frontend/modules/blockx/templates/indexSuccess.json.php
+++
@@ -1,7 +0,0 @@
-<?php $a = array() ?>
-
-<?php foreach ($blocks as $block): ?>
-	<?php $a[] = $block->toArray()->getRawValue() ?>
-<?php endforeach ?>
-
-<?php echo json_encode($a) ?>
